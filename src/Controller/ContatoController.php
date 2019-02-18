9eae4a4b884c6b5db670796b876255eb4ae6b049
Camila Fernandes <camilafernandesdev@gmail.com>
2019-02-18 20:56:58 -0300

    primeiro crud

--- src/Controller/ContatoController.php
+++ src/Controller/ContatoController.php
@@ -59,6 +59,55 @@ class ContatoController extends AbstractController
         return $this->render('contato/view.html.twig', [
             'contato' => $contato
         ]);
+    }
+
+    /**
+     * @Route("contato/editar/{id}", name="editar_contato")
+     */
+
+     public function update(Request $request, $id)
+     {
+        $em = $this->getDoctrine()->getManager();
+
+        $contato = $em->getRepository(Contato::class)->find($id);
+        
+        $form = $this->createForm(ContatoType::class, $contato);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em->persist($contato);
+            $em->flush();
+
+            $this->addFlash('success', 'Contato alterado com sucesso!');
+            return $this->redirectToRoute('contato_list');
+        }
+
+        return $this->render('contato/update.html.twig', [
+            'form' => $form->createView(),
+            'contato' => $contato
+        ]);
+
+     }
+
+     /**
+      * @Route("contato/delete/{id}", name="apagar_contato")
+      */
+    public function delete(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contato = $em->getRepository(Contato::class)->find($id);
+
+        if ($contato) {
+            $em->remove($contato);
+            $em->flush();
+
+            $this->addFlash("success", "Contato removido com sucesso!");    
+            return $this->redirectToRoute('contato_list');
+        }
+
+        $this->addFlash("error", "Contato não encontrado!");    
+        return $this->redirectToRoute('contato_list');
 
     }
 }
